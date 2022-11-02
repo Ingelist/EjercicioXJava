b5e368068b1d8b46d8637320799f47490ce180e1
Ingelist <betuel.turrubiarte@gmail.com>
2022-11-01 21:24:17 -0600

    Initial commit ejercicio tipos de datos en java

---
+++ src/com/ejercicio/HolaRaza.java
@@ -0,0 +1,23 @@
+package com.ejercicio;
+
+public class HolaRaza {
+    public static void main(String[] args) {
+
+
+        int enteros = 0;
+        String texto = "texto";
+        float flotante = 1.5f;
+        double doble = 1.20d;
+        char caracter = '@';
+        byte miByte = 2;
+        long myLong = 100;
+        short myShort = 10;
+        boolean booleano = true;
+
+
+        System.out.println(texto + "\n " + flotante + "\n " +
+                doble + " \n" + enteros +" \n " +  caracter +
+                " \n " + miByte + "\n" + myLong + " \n" +
+                myShort + " \n" + booleano);
+    }
+}
